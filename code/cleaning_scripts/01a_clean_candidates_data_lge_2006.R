79f212d79a3ecc4e62c0438611162788cf894f33
Patrick Pierson <patrickpierson19@gmail.com>
2021-09-29 10:16:30 -0400

    Adds code to aggregate variables to municipal geography (prev. code aggregated exclusively at ward level)

--- code/cleaning_scripts/01a_clean_candidates_data_lge_2006.R
+++ code/cleaning_scripts/01a_clean_candidates_data_lge_2006.R
@@ -168,8 +168,119 @@ data_final <- aw_interpolate(ward_2016_shape_file,
 
 data_final <- data_final %>% 
   mutate(electoral_cycle = 2006) %>% 
-  mutate(electo ral_cycle = as.factor(electoral_cycle))
+  mutate(electoral_cycle = as.factor(electoral_cycle))
 
 
 # WRITE TO PROCESSED_DATA FOLDER -----------------------
 saveRDS(data_final, here("data", "processed_data", "candidates_clean_2006_interpolated_to_2016.RDS"))
+
+
+
+##################################################################################
+##################################################################################
+######################### AGGREGATE TO MUNICIPAL LEVEL ###########################
+##################################################################################
+##################################################################################
+
+
+data_summary <- data %>% 
+  group_by(local_municipality_id) %>% 
+  mutate(sum_candidates = n()) %>% 
+  ungroup() %>% 
+  group_by(local_municipality_id) %>% 
+  mutate(num_male = sum(length(which(gender=="M"))),
+         num_female = sum(length(which(gender=="F")))) %>% 
+  ungroup() %>% 
+  group_by(local_municipality_id) %>% 
+  mutate(mean_age = mean(age, na.rm = TRUE)) %>% 
+  mutate(prop_female = 1 - (num_male / sum_candidates)) %>% 
+  mutate(num_independents = sum(length(which(party_name == "INDEPENDENT CANDIDATE")))) %>% 
+  mutate(num_ind_female = sum(length(which(party_name == "INDEPENDENT CANDIDATE" & gender=="F")))) %>% 
+  mutate(num_ind_male = sum(length(which(party_name == "INDEPENDENT CANDIDATE" & gender=="M")))) %>% 
+  distinct(local_municipality_id, .keep_all = TRUE) %>% 
+  select(province, local_municipality_id, local_municipality_name,
+         sum_candidates, num_male, num_female, mean_age, prop_female, num_independents,
+         num_ind_female, num_ind_male)
+
+
+data_num_parties <- data %>% 
+  filter(party_name != "INDEPENDENT CANDIDATE") %>% 
+  group_by(local_municipality_id) %>% 
+  mutate(num_parties_contest = n_distinct(party_name)) %>% 
+  distinct(local_municipality_id, .keep_all = TRUE) %>% 
+  select(local_municipality_id, num_parties_contest)
+
+
+data_summary <- left_join(data_summary, data_num_parties, by = "local_municipality_id")
+
+
+
+##############################################
+######## READ IN MUNICIPAL SHAPE FILE ########
+##############################################
+
+mun_shape_file <- st_read(here("data", "gis_data_raw", "LocalMunicipalities2006.shp"))
+
+mun_shape_file <- st_transform(mun_shape_file, 6148) #transform to SA's official coordinate system, Hartebeesthoek94 (code 6148)
+
+
+mun_shape_file <- mun_shape_file %>% 
+  rename(province = PROVINCE,
+         local_municipality_id = CAT_B) %>% 
+  select(province, local_municipality_id, geometry)
+
+
+# join candidates data to shape file
+data_join <- left_join(data_summary, mun_shape_file, by = "local_municipality_id") 
+
+
+#####################################################
+######## INTERPOLATE TO 2016 WARD BOUNDARIES ########
+#####################################################
+
+mun_2016_shape_file <- st_read(here("data", "gis_data_raw", "ecc03798-c4f1-42e2-a0e6-573c0c07c8542020410-1-1psvop3.0vyd.shp"))
+
+# TRANSFORM SHAPE FILES TO SA'S OFFICIAL COORDINATE SYSTEM -----------------------
+mun_2016_shape_file <- st_transform(mun_2016_shape_file, 6148) #transform to SA's official coordinate system, Hartebeesthoek94 (code 6148)
+
+
+mun_2016_shape_file <- mun_2016_shape_file %>% 
+  rename(local_municipality_id = LocalMunic) %>% 
+  select(local_municipality_id, geometry)
+
+
+# CONVERT DATA TO SF OBJECTS IN ORDER TO PERFORM AREAL INTERPOLATION -----------------------
+data_join <- st_as_sf(data_join)
+mun_2016_shape_file <- st_as_sf(mun_2016_shape_file)
+
+
+# VALIDATE GEOM -----------------------
+data_join <- st_make_valid(data_join)
+st_is_valid(data_join, reason = TRUE)
+
+mun_2016_shape_file <- st_make_valid(mun_2016_shape_file)
+st_is_valid(mun_2016_shape_file, reason=TRUE)
+
+
+# interpolate to 2016 polygons
+data_final <- aw_interpolate(mun_2016_shape_file,
+                             tid = local_municipality_id,
+                             source = data_join,
+                             sid = local_municipality_id,
+                             weight = "sum",
+                             output = "tibble",
+                             intensive = c("mean_age", "prop_female"),
+                             extensive = c("sum_candidates", "num_male", "num_female", "num_independents", "num_ind_female", "num_ind_male", "num_parties_contest")
+)
+
+ data_final <- data_final %>% 
+  mutate(electoral_cycle = 2006) %>% 
+  mutate(electoral_cycle = as.factor(electoral_cycle))
+
+
+# WRITE TO PROCESSED_DATA FOLDER -----------------------
+saveRDS(data_final, here("data", "processed_data", "candidates_clean_2006_interpolated_to_2016_MUNICIPALITY.RDS"))
+
+ 
+pat_test <- readRDS(here("data", "processed_data", "candidates_clean_2006_aggregated_to_mun_2016.RDS"))
+ 
