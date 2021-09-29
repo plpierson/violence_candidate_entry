38325ccaec463f9a3d02ff55c1f5dba2ce9a8776
Patrick Pierson <patrickpierson19@gmail.com>
2021-09-29 13:45:37 -0400

    Update script to include code for merging at municipal geography and creating municipal level dataset.

--- code/cleaning_scripts/02_merge_candidates_and_control_vars.R
+++ code/cleaning_scripts/02_merge_candidates_and_control_vars.R
@@ -63,3 +63,62 @@ summary(data_final$prop_female)
 
   # WRITE TO PROCESSED_DATA FOLDER -----------------------
 saveRDS(data_final, here("data", "processed_data", "full_candidate_data_with_controls.RDS"))
+
+
+############################################################################################
+############################################################################################
+######################### MERGE MUNICIPAL LEVEL DATA #######################################
+############################################################################################
+############################################################################################
+
+data_2006_mun <- readRDS(here("data", "processed_data", "candidates_clean_2006_interpolated_to_2016_MUNICIPALITY.RDS"))
+data_2011_mun <- readRDS(here("data", "processed_data", "candidates_clean_2011_interpolated_to_2016_MUNICIPALITY.RDS"))
+data_2016_mun <- readRDS(here("data", "processed_data", "candidates_clean_2016_MUNICIPALITY.RDS"))
+
+
+
+# do datasets look as expected?
+glimpse(data_2006_mun)
+glimpse(data_2011_mun)
+glimpse(data_2016_mun)
+
+# need to move a few things around to clean up for the merger
+data_2006_mun <- data_2006_mun %>% 
+  select(-c(num_ind_male, num_ind_female, num_independents, num_parties_contest))
+
+data_2011_mun <- data_2011_mun %>% 
+  select(-c(num_ind_male, num_ind_female, num_independents, num_parties_contest))
+
+data_2016_mun <- data_2016_mun %>% 
+  select(-c(local_municipality_name))
+
+
+
+##############################################
+######### READ IN CONTROL VARIABLES ##########
+##############################################
+controls_2006_mun <- readRDS(here("data", "processed_data", "data_2006_controls_MUN.RDS"))
+controls_2011_mun <- readRDS(here("data", "processed_data", "data_2011_controls_MUN.RDS"))
+controls_2016_mun <- readRDS(here("data", "processed_data", "data_2011_controls_MUN.RDS"))
+
+
+# merge with candidates data
+data_full_mun_2006 <- left_join(data_2006_mun, controls_2006_mun, by = "local_municipality_id")
+data_full_mun_2011 <- left_join(data_2011_mun, controls_2011_mun, by = "local_municipality_id")
+data_full_mun_2016 <- left_join(data_2016_mun, controls_2016_mun, by = "local_municipality_id")
+
+# bind datasets together
+data_final_mun <- rbind(data_full_mun_2006, data_full_mun_2011, data_full_mun_2016)
+
+
+# quick inspection to make sure things look ok
+length(unique(data_final_mun$local_municipality_id)) # 213
+table(data_final_mun$electoral_cycle) 
+
+summary(data_final_mun$sum_candidates)
+summary(data_final_mun$num_male)
+summary(data_final_mun$mean_age) 
+
+
+# WRITE TO PROCESSED_DATA FOLDER -----------------------
+saveRDS(data_final_mun, here("data", "processed_data", "full_candidate_data_with_controls_MUN.RDS"))
